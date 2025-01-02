e20c79550880dcbb401483717f47574cd0d25847
RifatKhan11 <rifat.v.gpi@gmail.com>
2025-01-02 14:41:10 +0600

    wdca

---
+++ PoliceOfficerManagement/Areas/EmployeeArea/Controllers/EmployeInfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PoliceOfficerManagement.Areas.EmployeeArea.Controllers
+{
+    public class EmployeInfoController : Controller
+    {
+        public IActionResult CreateEmployeeProfile()
+        {
+            return View();
+        }
+    }
+}
